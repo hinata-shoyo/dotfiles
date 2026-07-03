bb51bf7945534b1e0dfc8237930ef59c51c4cef4
Abhishek Sharma <avsharma4500@gmail.com>
2026-07-03 18:35:41 +0530

    Add template for C++ linked list implementation

---
+++ nvim/templates/cppll.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(1), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(NULL);
+
+  return 0;
+}
