d09c7ffcbf8a2548197236db7bcd9e962b7ecc94
Abhishek Sharma <avsharma4500@gmail.com>
2026-07-03 18:35:16 +0530

    Create cppbt.cpp

---
+++ nvim/templates/cppbt.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(NULL);
+
+
+  return 0;
+}
+
